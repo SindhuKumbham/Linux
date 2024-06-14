c0741b212a743ba4917de3a7d84c55ecccbafbcb
SindhuKumbham <kumbhamsindhu14@gmail.com>
2024-06-14 10:08:05 +0530

    code

---
+++ program/subtraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    int b;
+    int sub;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number:");
+    scanf("%d",&b);
+    sub=a-b;
+    printf("the subtraction of two numbers %d and %d is %d",a,b,sub);
+}
